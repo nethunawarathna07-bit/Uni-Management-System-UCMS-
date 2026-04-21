d27524013c4fc452204cfb1c55e2674153588394
nethunawarathna07-bit <nethunawarathna07@gmail.com>
2026-04-21 11:38:17 +0530

    Add files via upload

---
+++ Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿namespace UniManage.Models
+{
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public List<Submission> Submissions { get; set; }
+
+        // NEW: stores the file path if lecturer uploads a PDF
+        public string AssignmentFilePath { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,15 @@
+﻿namespace UniManage.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; }
+        public int MaxStudents { get; set; }
+        public string LecturerId { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+        public List<Assignment> Assignments { get; set; } = new();
+    }
+}
